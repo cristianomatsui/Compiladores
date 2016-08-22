23f7af41de26a0fd83eeaac51cdedadf79341920
Cristiano <cristiano.matsui@gmail.com>
2016-08-22 16:09:40 -0300

    Kinda working

--- Atividade_1.py
+++ Atividade_1.py
@@ -1,29 +1,32 @@
+def main():
+    def read_char(linha, coluna, char, counter):
+        file.seek(counter)
+        counter += 1
+        char = file.readline(1)
+        coluna += 1
+        if char == '\n':
+            linha += 1
+        return linha, coluna, char, counter
 
-def read_char(linha, coluna, char, counter):
-    file.seek(counter)
-    counter += 1
-    char = file.readline(1)
-    coluna += 1
-    if char == '\n':
-        linha += 1
-    return
-
-archive = input("Insira o nome do arquivo: ")
-file = open(archive, "r")
-char = file.read(1)
-linha = 1
-coluna = 30
-counter = 1
+    archive = input("Insira o nome do arquivo: ")
+    file = open(archive, "r")
+    char = file.read(1)
+    linha = 1
+    coluna = 1
+    counter = 1
 
 
-while char:
-    if char == '{':         #tratamento de comentários
-        while char != '}':
-            read_char(linha, coluna, char, counter)
-            if (not char):
-                print ("Erro: Linha %d Coluna %d, Operador '}' esperado." %(linha, coluna))
-                exit(-1)
+    while char:
+        if char == '{':         #tratamento de comentários
+            while char != '}':
+                linha, coluna, char, counter = read_char(linha, coluna, char, counter)
+                if (not char):
+                    print ("Erro: Linha %d Coluna %d, Operador '}' esperado." %(linha, coluna))
+                    exit("Error")
 
-    read_char(linha, coluna, char, counter)
+        linha, coluna, char, counter = read_char(linha, coluna, char, counter)
 
-file.close()
+    file.close()
+
+
+main()
